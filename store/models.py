7ad0204365ba68d33e6be70e376204ddcf85e164
Jose Delaguarda <josedelaguarda@macbook-pro-2.myfiosgateway.com>
2021-05-08 23:11:49 -0400

    renderedd for the store

--- store/models.py
+++ store/models.py
@@ -1,22 +1,35 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Customer(models.Model):
-    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200)
-    
+
     def __str__(self):
         return self.name
-    #boolean value if we want to know if the item is digital 
+    #boolean value if we want to know if the item is digital
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
-    digital = models.BooleanField(default=False,null=True, blank=True)
-    #image
+    digital = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
     def __str__(self):
         return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+            except:
+            url = ''
+            return url
     
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
